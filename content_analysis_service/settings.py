95a29fa424fc64d1c1aada85e8a33b4d9ceefd4c
Dan <daniftincaalexandru@gmail.com>
2020-05-31 00:36:26 +0300

    fixed gitignore, added a universal cors fixer

--- content_analysis_service/settings.py
+++ content_analysis_service/settings.py
@@ -112,9 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-CORS_ORIGIN_WHITELIST = ('http://localhost:3000',)
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ORIGIN_WHITELIST = ('http://localhost:3000',)
 
 
 # Internationalization
