e0f643b59e088c94e9a3757eb5ac462c6038e65b
Dan <daniftincaalexandru@gmail.com>
2020-05-31 00:28:38 +0300

    uncommented the code that was adding new jobs to the mq

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Daiana\Desktop\projects\content_analysis_service\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
